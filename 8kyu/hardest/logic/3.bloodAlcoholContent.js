c83c1cdcb5f88a3d55b47df52aa4328ef1f42c19
azmu0520 <azizbekbootcam@gmail.com>
2022-10-09 15:51:49 +0500

    done

--- 8kyu/hardest/logic/3.bloodAlcoholContent.js
+++ 8kyu/hardest/logic/3.bloodAlcoholContent.js
@@ -1,3 +1,11 @@
-function bloodAlcoholContent(drinks, weight, sex, time) {
-  return bac;
+function bloodAlcoholContent({ ounces, abv }, weight, sex, time) {
+  // BAC% = (A × 5.14 / W × r) - .015 × H
+  sex = sex == 'male' ? 0.73 : 0.66;
+  //
+  let res = (((ounces * abv * 5.14) / weight) * sex - 0.015 * time).toFixed(4);
+  //
+  return +res;
+  //  return parseFloat(((drinks.ounces * drinks.abv * 5.14 / weight * (sex == 'male' ? 0.73 : 0.66)) - 0.015 * time).toFixed(4));
 }
+
+bloodAlcoholContent({ ounces: 50, abv: 0.14 }, 250, 'male', 3);
